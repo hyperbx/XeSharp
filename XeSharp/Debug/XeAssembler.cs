44e0933b9dc4006b7955cff9c0c9356ff2948ab5
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-02-26 03:10:37 +0000

    Added Keystone for PPC assembler

---
+++ XeSharp/Debug/XeAssembler.cs
@@ -0,0 +1,21 @@
+﻿using Keystone;
+
+namespace XeSharp.Debug
+{
+    public class XeAssembler
+    {
+        public static Architecture Architecture => Architecture.PPC;
+        public static Mode Mode => Mode.BIG_ENDIAN | Mode.PPC64;
+
+        /// <summary>
+        /// Assembles instructions into bytecode.
+        /// </summary>
+        /// <param name="in_asm">The instructions to assemble.</param>
+        /// <param name="in_addr">The address for context for the assembler.</param>
+        public static byte[] Assemble(string in_asm, uint in_addr = 0)
+        {
+            using (var engine = new Engine(Architecture, Mode) { ThrowOnError = true })
+                return engine.Assemble(in_asm, in_addr).Buffer;
+        }
+    }
+}
